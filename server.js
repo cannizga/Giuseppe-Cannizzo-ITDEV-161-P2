7e663138428a47092b2bb07fed2d9f6ab84ee412
cannizga <giuseppeacannizzojr@gmail.com>
2021-09-19 20:59:48 -0500

    Update

--- server.js
+++ server.js
@@ -7,10 +7,28 @@ const app = express();
 // Connect Database
 connectDatabase();
 
+//Configure Middleware
+connectDatabase();
+
+// API Endpoints
+/**
+ *  @route GET /
+ *  @desc Test Endpoint
+ */
+
 // API endpoints
 app.get('/', (req, res) => 
     res.send("http get request sent to root api endpoint")
     );
+
+/**
+ *  @route POST api/users
+ *  @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 // Connection listener
 app.listen(3000, () => console.log(`Express server running on port 3000`));
