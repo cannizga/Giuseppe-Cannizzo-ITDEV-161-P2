42fac6bc6c5d827c3fd5648b3899ff4e1380f930
cannizga <giuseppeacannizzojr@gmail.com>
2021-09-22 20:11:54 -0500

    Activity 4 Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000').then((response) => {
+      this.setState({
+        data: response.data
+      })
+    })
+
+    .catch((error) => {
+      console.error(`Error fetching data: ${error}`);
+    })
+  }
+
   render(){
     return (
       <div className = "App" >
