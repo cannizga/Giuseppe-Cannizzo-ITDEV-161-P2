0774341b3ca93194db5b00f61d90a33ea58d1a1c
cannizga <giuseppeacannizzojr@gmail.com>
2021-11-08 22:45:15 -0600

    activity 8

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken"
 import config from "config"
 import User from './models/User';
+import auth from './middleware/auth';
 
 //Initialize express application
 const app = express();
@@ -27,6 +28,15 @@ app.use(cors({ origin: 'http://localhost:3000'}));
 app.get('/', (req, res) => 
     res.send("http get request sent to root api endpoint")
     );
+
+   /* app.get('./api/auth', auth, async (req, res) => {
+        try {
+            const user = await User.findById(req.user.id);
+            res.status(200).json(user);
+        } catch (error) {
+            res.status(500).send('Unknown server error');
+        }
+    });*/
 
 /**
  *  @route POST api/users
