ed18a392ca5d8632b1b45aeadc264e489a0f47dc
cannizga <giuseppeacannizzojr@gmail.com>
2021-11-09 21:10:44 -0600

    Activity-8 Completed!

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { application } from 'express';
 import connectDatabase from './Config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
@@ -85,12 +85,55 @@ app.post(
 
             await user.save();
 
-            const payload = {
-                user: {
-                    id: user.id
-                }
+            returnToken(user, res);
+            } catch (error) {
+                res.status(500).send('Server error');
             };
+        }
+    }
+);
 
+            app.get('/api/auth', auth, async (req, res) => {
+                try{
+                    const user = await User.findById(req.user.id);
+                    res.status(200).json(user);
+                }   catch (error) {
+                    res.status(500).send('Unknown server error');
+                }
+            });
+
+            app.post('/api/login',[ check('email', 'Please enter a valid email').isEmail(), check('password', 'A password is required').exists()],
+                    async (req, res) => {
+                        const errors = validationResult(req);
+                        if(!errors.isEmpty()) {
+                            return res.status(422).json({ errors: errors.array() });
+                        } else {
+                            const { email, password } = req.body;
+                            try{
+                                let user = await User.findOne({ email: email});
+                                if(!user) {
+                                    return res.status(400).json({errors: [{msg: 'Invalid email or password'}] });
+                                }
+
+                                const match = await bcrypt.compare(password, user.password);
+                                if(!match) {
+                                    return res.status(400).json({errors: [{ msg: 'Invalid email or password'}] });
+                                }
+
+                                returnToken(user, res);
+                            } catch(error) {
+                                res.status(500).send('Server error');
+                            }
+                        }
+                    });
+
+            const returnToken = (user, res) => {
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                }
+            
             jwt.sign(
                 payload,
                 config.get("jwtSecret"),
@@ -99,17 +142,8 @@ app.post(
                     if (err) throw err;
                     res.json({token: token})
                 }
-            )
-
-            res.send("User successfully registered");
-            } 
-            catch (error) {
-                res.status(500).send('Server error');
-            }
-        }
-    }
-);
-
+            );
+        };
 // Connection listener
 const port = 5000;
 app.listen(port, () => console.log(`Express server running on port 3000 ${port}`));
